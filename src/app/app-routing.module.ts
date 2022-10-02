c324a6ff40dde0028e5f137295733666e9083f28
LizardLeliel <shadow.shade.shine@gmail.com>
2022-10-02 14:23:05 -0300

    Removed many pages; I only need two pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-// import { ComponentTestComponent } from './app/pages/component-test/component-test.ts'
 import { PagesModule } from './pages/pages.module';
 
-import { ComponentTestComponent }  from './pages/component-test/component-test.component';
 import { AboutMeComponent }        from './pages/about-me/about-me.component';
-import { ProgrammingComponent }    from './pages/programming/programming.component';
-import { WebDevelopmentComponent } from './pages/web-development/web-development.component';
 import { ArtCommissionComponent }  from './pages/art-commission/art-commission.component';
 import { NotFoundComponent }       from './pages/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '',                component: AboutMeComponent },
-  { path: 'test',            component: ComponentTestComponent },
-  { path: 'programming',     component: ProgrammingComponent },
-  { path: 'web-development', component: WebDevelopmentComponent },
   { path: 'art',             component: ArtCommissionComponent },
   { path: '**',              component: NotFoundComponent },
 ];
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ComponentTestComponent } from './component-test/component-test.component';
 import { AboutMeComponent } from './about-me/about-me.component';
-import { ProgrammingComponent } from './programming/programming.component';
-import { WebDevelopmentComponent } from './web-development/web-development.component';
 import { ArtCommissionComponent } from './art-commission/art-commission.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
@@ -15,10 +12,7 @@ import { WideCardRowComponent } from '../components/wide-card-row/wide-card-row.
 
 @NgModule({
   declarations: [
-    ComponentTestComponent,
     AboutMeComponent,
-    ProgrammingComponent,
-    WebDevelopmentComponent,
     ArtCommissionComponent,
     NotFoundComponent,
 
@@ -29,10 +23,7 @@ import { WideCardRowComponent } from '../components/wide-card-row/wide-card-row.
     CommonModule
   ],
   exports: [
-    ComponentTestComponent,
     AboutMeComponent,
-    ProgrammingComponent,
-    WebDevelopmentComponent,
     ArtCommissionComponent,
     NotFoundComponent
   ]
